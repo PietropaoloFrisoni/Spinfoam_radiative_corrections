e381227880335c500ba887273bef4fbf4845894b
PietropaoloFrisoni <frisus@outlook.it>
2022-06-13 21:54:40 +0200

    goat 2 data and cleaned some stuff

--- CC_script_propagator.sh
+++ CC_script_propagator.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -A def-vidotto
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=3
-#SBATCH --mem-per-cpu=4G
-#SBATCH --time=1-0:00:00
-#SBATCH --job-name=frogface_2
-#SBATCH --output=CLUSTER_COMPUTATIONS/frogface_2.log
-#SBATCH --error=CLUSTER_COMPUTATIONS/frogface_2.err
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=6G
+#SBATCH --time=0-6:00:00
+#SBATCH --job-name=frogface_prop
+#SBATCH --output=CLUSTER_COMPUTATIONS/frogface_prop.log
+#SBATCH --error=CLUSTER_COMPUTATIONS/frogface_prop.err
 #SBATCH --mail-type=BEGIN,FAIL,END
 #SBATCH --mail-user=pfrisoni@uwo.ca
 
@@ -32,7 +32,7 @@ SHELL_MAX=10
 IMMIRZI=0.1
 STORE_FOLDER=${BASE_DIR}/CLUSTER_COMPUTATIONS
 BSPIN=0.5
-CODE_TO_RUN=frogface_2_EPRL
+CODE_TO_RUN=frogface_EPRL_propagator_matrix
 
 echo "Running on: $SLURM_NODELIST"
 echo
@@ -44,24 +44,24 @@ cp ${FASTWIG_TABLES_PATH}/* $SLURM_TMPDIR/
 
 
 
-#echo "Extracting previous boosters to: $SLURM_TMPDIR ..."
-#echo
+echo "Extracting previous boosters to: $SLURM_TMPDIR ..."
+echo
 
-#tar -xvf ${BOOSTER_DIR}/${CODE_TO_RUN}_SHELL_MIN_${SHELL_MIN}_SHELL_MAX_${SHELL_MAX}_IMMIRZI_${IMMIRZI}_CUTOFF_${CUTOFF}.tar.gz -C $SLURM_TMPDIR/
+tar -xvf ${BOOSTER_DIR}/${CODE_TO_RUN}_SHELL_MIN_${SHELL_MIN}_SHELL_MAX_${SHELL_MAX}_IMMIRZI_${IMMIRZI}_CUTOFF_${CUTOFF}.tar.gz -C $SLURM_TMPDIR/
 
 
 
 echo "Running: ${CODE_TO_RUN}"
 echo
 
-${JULIA_DIR}/bin/julia -p $SLURM_TASKS_PER_NODE --threads $SLURM_CPUS_PER_TASK ${BASE_DIR}/julia_codes/${CODE_TO_RUN}.jl $SLURM_TMPDIR ${CUTOFF} ${SHELL_MIN} ${SHELL_MAX} ${IMMIRZI} ${STORE_FOLDER}
+${JULIA_DIR}/bin/julia -p $SLURM_TASKS_PER_NODE --threads $SLURM_CPUS_PER_TASK ${BASE_DIR}/julia_codes/${CODE_TO_RUN}.jl $SLURM_TMPDIR ${CUTOFF} ${SHELL_MIN} ${SHELL_MAX} ${IMMIRZI} ${BSPIN} ${STORE_FOLDER}
 
 
 
 echo "Compressing and copying computed boosters to ${BOOSTER_DIR}..."
 echo
 
-tar -czvf ${BOOSTER_DIR}/${CODE_TO_RUN}_SHELL_MIN_${SHELL_MIN}_SHELL_MAX_${SHELL_MAX}_IMMIRZI_${IMMIRZI}_CUTOFF_${CUTOFF}.tar.gz $SLURM_TMPDIR/vertex
+tar -czvf ${BOOSTER_DIR}/${CODE_TO_RUN}_SHELL_MIN_${SHELL_MIN}_SHELL_MAX_${SHELL_MAX}_IMMIRZI_${IMMIRZI}_CUTOFF_${CUTOFF}_BSPIN_${BSPIN}.tar.gz $SLURM_TMPDIR/vertex
 
 
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Radiative corrections to the EPRL spin foam propagator
 
-**_The Julia codes are parallelized on the available cores using a hybrid multilevel parallelization scheme, exploiting the available processes, threads and loop vectorization._** It is therefore advisable for the performance to use a number of workers \* threads equal to or less than the physical number of cores present on the system.
+**_The Julia codes are parallelized on the available cores. In some cases we adopted a hybrid multilevel parallelization scheme, exploiting the available processes, threads and loop vectorization._** It is therefore advisable for the performance to use a number of workers \* threads equal to or less than the physical number of cores present on the system.
 
 A full list of the employed Julia packages can be found in `./julia_codes/pkgs.jl`. **Before executing the codes, all packages must be installed.**
 
